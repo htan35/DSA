4d7fd986eca949be3f2b34df2023f23e8eb9597d
Harsh Tanwar <156991396+htan35@users.noreply.github.com>
2026-09-21 12:48:57 +0530

    Create DP 2. Climbing Stairs (optimization).cpp

---
+++ dp/DP 2. Climbing Stairs/DP 2. Climbing Stairs (optimization).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int prev2 = 1;
+        int prev = 2;
+
+        for(int i = 3; i <= n; i++) {
+            int curr = prev + prev2;
+
+            prev2 = prev;
+            prev = curr;
+        }
+        // not returning cur because prev has curr at last
+        return prev;
+    }
+};
